53ec44d64166f465ef4cd9f6ac2103ccddeeb6e4
Artem Starodubtsev <73162396+ayostar@users.noreply.github.com>
2022-09-27 17:08:00 +0300

    handler modified

--- AJS-TASK-2/destructuring/src/js/handler.js
+++ AJS-TASK-2/destructuring/src/js/handler.js
@@ -3,12 +3,10 @@ const specialProps = 'special';
 export default function getSpecialAttack(obj) {
   if (Object.prototype.hasOwnProperty.call(obj, specialProps)) {
     const resultArray = new Array();
-    const {
-      [specialProps]: [...specialObjects],
-    } = obj;
+    const { special } = obj;
 
-    if (specialObjects.length) {
-      specialObjects.forEach((specialElement) => {
+    if (special.length) {
+      special.forEach((specialElement) => {
         const {
           id,
           name,
@@ -27,6 +25,34 @@ export default function getSpecialAttack(obj) {
     return resultArray;
   } else return [];
 }
+
+// export default function getSpecialAttack(obj) {
+//   if (Object.prototype.hasOwnProperty.call(obj, specialProps)) {
+//     const resultArray = new Array();
+//     const {
+//       [specialProps]: [...specialObjects],
+//     } = obj;
+
+//     if (specialObjects.length) {
+//       specialObjects.forEach((specialElement) => {
+//         const {
+//           id,
+//           name,
+//           description = 'Описание недоступно',
+//           icon,
+//         } = specialElement;
+
+//         resultArray.push({
+//           id,
+//           name,
+//           description,
+//           icon,
+//         });
+//       });
+//     }
+//     return resultArray;
+//   } else return [];
+// }
 
 // export default function getSpecialAttack(obj) {
 //   const resultArray = new Array();
